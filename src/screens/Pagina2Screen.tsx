5174b459e72256bbc1fc2cb9707b9bd1f5b7601a
Agu <alemosmusi@gmail.com>
2023-08-04 19:08:31 -0300

    ahora si toda la navegacion

---
+++ src/screens/Pagina2Screen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Pagina2Screen = () => {
+  return (
+    <View>
+        <Text> Pagina2screen</Text>
+    </View>
+  )
+}
+
+export default Pagina2Screen
---
+++ src/screens/Pagina3Screen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Pagina3Screen = () => {
+  return (
+    <View>
+        <Text> Pagina3screen</Text>
+    </View>
+  )
+}
+
+export default Pagina3Screen
